8418483a1979a49efd7544cd141a3ad34e10e33e
A. Unique TensorFlower <gardener@tensorflow.org>
2026-04-30 03:26:48 -0700

    Automated Code Change
    PiperOrigin-RevId: 908068984

--- tensorflow/c/experimental/saved_model/core/tf_saved_model_api.cc
+++ tensorflow/c/experimental/saved_model/core/tf_saved_model_api.cc
@@ -24,6 +24,7 @@ limitations under the License.
 #include "absl/container/flat_hash_map.h"
 #include "absl/log/log.h"
 #include "absl/status/status.h"
+#include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/optional.h"
 #include "tensorflow/c/eager/immediate_execution_context.h"
