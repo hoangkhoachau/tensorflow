3077e72f902f98499b8093aebbcd28ecdffdfcae
Mikhail Goncharov <goncharov@google.com>
2026-05-04 02:00:57 -0700

    [XLA:GPU] disable xla_gpu_unsupported_enable_triton_multi_output_fusion by default
    New tiling analysis does not yet support multi output fusions and we are considering to disable it for now to see if it makes significant difference in performance.
    
    PiperOrigin-RevId: 909886521

--- third_party/xla/xla/codegen/xtile/codegen/fusion_emitter.cc
+++ third_party/xla/xla/codegen/xtile/codegen/fusion_emitter.cc
@@ -1189,6 +1189,12 @@ absl::StatusOr<mlir::OwningOpRef<mlir::ModuleOp>> EmitXTileModule(
     const std::optional<stream_executor::GpuComputeCapability>& gpu_cc) {
   const auto debug_options = fusion.GetModule()->config().debug_options();
 
+  if (fusion.IsMultiOutputFusion() &&
+      !debug_options
+           .has_xla_gpu_unsupported_enable_triton_multi_output_fusion()) {
+    return absl::InvalidArgumentError("Multi-output fusion is disabled.");
+  }
+
   const HloComputation* hlo_computation =
       fusion.fused_instructions_computation();
 
